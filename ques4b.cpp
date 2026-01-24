5a69b61ee78944d4837572b750fb90eea976b11f
Bhavika <bhavika2467@gmail.com>
2026-01-24 23:37:55 +0530

    Add files via upload

---
+++ ques4b.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+//#include<iomanip>
+using namespace std ;
+int main (){
+int item_num,quantity,unit_price,discount_amt;
+cin>>item_num>>quantity>>unit_price;
+cout<<"item number :"<<item_num<<endl<<"quantity"<<quantity<<endl<<"unit price"<<unit_price<<endl;
+
+
+
+}
---
+++ ques7.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std ;
+int main() {
+    int x,y,z;
+    cout<<"enter 3 scores :";
+    cin>>x>>y>>z;
+    if(x>y&&x>z) {
+        cout<<"";
+    }
+}
